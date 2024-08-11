875af8fdacb4f8aed626c212cbf3d059fe4d8164
mwcui <matthew.w.cui@gmail.com>
2024-08-10 23:12:34 -0700

    testing the html body

--- src/app/Components/toggleCheckeredBackground.js
+++ src/app/Components/toggleCheckeredBackground.js
@@ -12,7 +12,20 @@
 //   }
   
 
-export function toggleCheckeredBackground(isOpen) {
+// export function toggleCheckeredBackground(isOpen) {
+//     const htmlElement = document.documentElement;
+//     const bodyElement = document.body;
+  
+//     if (isOpen) {
+//       htmlElement.classList.add('checkered-bg');
+//       bodyElement.classList.add('checkered-bg');
+//     } else {
+//       htmlElement.classList.remove('checkered-bg');
+//       bodyElement.classList.remove('checkered-bg');
+//     }
+//   }
+
+  export function toggleCheckeredBackground(isOpen) {
     const htmlElement = document.documentElement;
     const bodyElement = document.body;
   
