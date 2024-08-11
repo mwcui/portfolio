799325163c989e94e2675c573dbb2342e80b69fb
mwcui <matthew.w.cui@gmail.com>
2024-08-10 23:22:18 -0700

    menu tweaks

--- src/app/Components/menu.js
+++ src/app/Components/menu.js
@@ -5,12 +5,12 @@ const Menu = ({ closeMenu }) => {
       <div className="bg-white w-full max-w-[calc(100vw-120px)] h-full max-h-[calc(100vh-120px)] text-center relative border-8 border-[#0C3B65] flex flex-col justify-center p-4">
         <button
           onClick={closeMenu}
-          className="absolute top-4 right-4 text-3xl font-inika hover:text-gray-600"
+          className="absolute top-4 right-6 text-3xl font-inika hover:text-gray-600"
         >
           X
         </button>
-        <h1 className="text-4xl font-bold font-inika mb-8">MENU</h1>
-        <ul className="space-y-6 text-2xl font-inika">
+        <h1 className="text-5xl font-bold font-inika mb-10">MENU</h1>
+        <ul className="space-y-6 text-3xl font-inika">
           <li className="hover:text-gray-600 cursor-pointer">EXPERIENCE</li>
           <li className="hover:text-gray-600 cursor-pointer">CREATIVE</li>
           <li className="hover:text-gray-600 cursor-pointer">ABOUT</li>
