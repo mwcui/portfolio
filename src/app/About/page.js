1fc8893b8937a7d1196c2419d312b2f3bfbdba17
mwcui <matthew.w.cui@gmail.com>
2024-08-21 13:52:37 -0700

    about page updates

--- src/app/About/page.js
+++ src/app/About/page.js
@@ -17,9 +17,9 @@ import profilePic from '/public/About/ProfilePicture.png'; // Replace with your 
 
 const About = () => {
   return (
-    <div className="flex flex-col items-center text-white font-inika py-10 px-4">
+    <div className="flex flex-col items-center text-white font-inika py-10">
 
-      <div className="mb-8 w-60 h-60 relative">
+      <div className="m-10 w-44 h-44 relative">
         <Image
           src={profilePic}
           alt="Profile Picture of Matthew"
@@ -29,9 +29,9 @@ const About = () => {
       </div>
 
       {/* About Text */}
-      <div className="text-center text-2xl max-w-2xl leading-relaxed">
+      <div className="text-center text-2xl max-w-2xl leading-relaxed" style={{ width: '600px', maxWidth: '100vw' }}>
         <p>
-          Matthew is an engineer based in Los Angeles, California. His work spans many companies including Symphony Markets, Northrop Grumman, The Walt Disney Company, and NASA.
+          Matthew is an entrepreneur and engineer based in Los Angeles, California. His work spans many organizations including NASA, The Walt Disney Company, and Northrop Grumman.
         </p>
         <p className="mt-4">
           He is currently building gov-tech products to help local governments across the country better serve their residents.
