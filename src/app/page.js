e05de915ed4a410063761c633bca2556041d10b8
mwcui <matthew.w.cui@gmail.com>
2024-08-29 12:05:24 -0700

    the fade in effect is beautiful :))

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,52 @@
 // this works with 277 - 303 block effect
 
+// "use client";
+
+// import { useState } from 'react';
+// import BlockEffect from './Components/BlockEffect.js';
+// import Template from './Components/template.js';
+// import Logo from './Components/logo';
+// import Creative from './Creative/page.js';
+// import Experience from './Experience/page.js';
+// import Contact from './Contact/page.js';
+// import About from './About/page';
+
+
+// const componentsMap = {
+//   creative: Creative,
+//   experience: Experience,
+//   contact: Contact,
+//   about: About,
+// };
+
+// const Home = () => {
+//   const [currentComponent, setCurrentComponent] = useState('logo'); // Start with Logo
+
+//   const handleMenuSelection = (selection) => {
+//     setCurrentComponent(selection);
+//   };
+
+//   const CurrentComponent = componentsMap[currentComponent] || Logo;
+
+//   return (
+//     <div>
+//       <Template onMenuSelect={handleMenuSelection}>
+//         <CurrentComponent />
+//       </Template>
+//       <BlockEffect onComplete={() => {}} />
+//     </div>
+//   );
+// };
+
+// export default Home;
+
+
+
+
+
 "use client";
 
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import BlockEffect from './Components/BlockEffect.js';
 import Template from './Components/template.js';
 import Logo from './Components/logo';
@@ -10,7 +54,6 @@ import Creative from './Creative/page.js';
 import Experience from './Experience/page.js';
 import Contact from './Contact/page.js';
 import About from './About/page';
-
 
 const componentsMap = {
   creative: Creative,
@@ -22,6 +65,16 @@ const componentsMap = {
 const Home = () => {
   const [currentComponent, setCurrentComponent] = useState('logo'); // Start with Logo
 
+  useEffect(() => {
+    const content = document.getElementById("page-content");
+    content.style.transition = "opacity 0.5s ease-in-out"; // Add transition for opacity
+    content.style.opacity = "0"; // Start with the content hidden
+
+    setTimeout(() => {
+      content.style.opacity = "1"; // Fade in the content after a delay
+    }, 1500); // Delay matches the block effect animation duration
+  }, []);
+
   const handleMenuSelection = (selection) => {
     setCurrentComponent(selection);
   };
@@ -30,9 +83,11 @@ const Home = () => {
 
   return (
     <div>
-      <Template onMenuSelect={handleMenuSelection}>
-        <CurrentComponent />
-      </Template>
+      <div id="page-content">
+        <Template onMenuSelect={handleMenuSelection}>
+          <CurrentComponent />
+        </Template>
+      </div>
       <BlockEffect onComplete={() => {}} />
     </div>
   );
