669e9dd32e11dbfe3058d98a44b9b26f738554d4
mwcui <matthew.w.cui@gmail.com>
2024-09-10 10:24:53 -0700

    logo priority added. Testing the mouse effect

--- src/app/Logo/page.js
+++ src/app/Logo/page.js
@@ -1,8 +1,5 @@
-
-
 "use client"
 
-import { useEffect } from 'react';
 import Image from 'next/image';
 import logo from '/public/logo.svg';
 // import { useLogoVisibility } from './LogoVisibilityContext';
@@ -10,7 +7,15 @@ import logo from '/public/logo.svg';
 const Logo = () => {
   return (
     <div className="flex items-center justify-center flex-grow z-10000">
-      <Image src={logo} alt="Logo" width={700} height={700} className="object-contain" />
+      <Image 
+        src={logo} 
+        alt="Logo" 
+        width={700} 
+        height={700} 
+        className="object-contain" 
+        priority
+        loading="eager"
+      />
     </div>
   );
 };
